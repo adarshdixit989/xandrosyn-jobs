af09348c66df594276ccac59562b21e8fb864a88
adarshdixit989 <119063821+adarshdixit989@users.noreply.github.com>
2026-09-20 10:19:28 +0530

    feat: add backend/app/db.py

---
+++ backend/app/db.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+engine = create_engine(os.getenv("DATABASE_URL", "sqlite:///./xandrosyn.db"), pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
